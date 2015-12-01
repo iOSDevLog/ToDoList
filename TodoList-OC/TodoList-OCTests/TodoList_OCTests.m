9e24ce5ddd5b3fde5756379c9882dd976563fe7c
iosdevlog <iosdevlog@iosdevlog.com>
2015-12-01 23:16:25 +0800

    OC ToDoList

---
+++ TodoList-OC/TodoList-OCTests/TodoList_OCTests.m
@@ -0,0 +1,39 @@
+//
+//  TodoList_OCTests.m
+//  TodoList-OCTests
+//
+//  Created by iosdevlog on 15/12/1.
+//  Copyright © 2015年 iosdevlog. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TodoList_OCTests : XCTestCase
+
+@end
+
+@implementation TodoList_OCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
